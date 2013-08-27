6e48712fd7bcec4506b7a82d543c363e830a907f
kinal88 <kinal88@gmail.com>
2013-08-26 20:58:35 -0700

    add age to person object and update program

--- contacts.rb
+++ contacts.rb
@@ -4,6 +4,7 @@ class Person
   attr_accessor :first_name
   attr_accessor :last_name
   attr_accessor :phone
+  attr_accessor :age
 
   def full_name
     first_name + " " + last_name
@@ -16,12 +17,13 @@ class Contacts
 
     loop do
       command = ask("> ")
-
+ 
       if command == "list"
-        puts "Name".ljust(30) + "Phone".rjust(12)
-        puts "-" * 42
+        puts "Name".ljust(30) + "Phone".rjust(12) + "Age".rjust(15)
+        puts "-" * 60
         people.each do |person|
-          puts person.full_name.ljust(30) + person.phone.rjust(12)
+          puts person.full_name.ljust(30) + person.phone.rjust(12) + person.age.rjust(17)
+
         end
 
       elsif command == "add"
@@ -30,6 +32,7 @@ class Contacts
         person.first_name = ask("First Name: ")
         person.last_name  = ask("Last Name: ")
         person.phone      = ask("Phone: ")
+        person.age        = ask("Age: ")
 
         people.push(person)
       elsif command == "quit"
